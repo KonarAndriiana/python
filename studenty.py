edccaec91efc60417c1040b7c3ca1db3a40e4675
KonarAndriiana <andriiana.konar@kosickaakademia.sk>
2024-09-24 10:15:58 +0200

    Add functions to sort students by average and update grades

--- studenty.py
+++ studenty.py
@@ -24,6 +24,21 @@ def add_student(student_list, new_student):
     print(f"Student {new_student[0]} added")
     return student_list
 
+def sort_students_by_avg(student_list):
+    averages = average_grade(student_list)
+    sorted_students = sorted(averages, key=lambda x: x[1], reverse=True)
+    print("Sorted students by average:", sorted_students)
+    return sorted_students
+
+def update_grade(student_list, name, subject_idx, new_grade):
+    for i, student in enumerate(student_list):
+        if student[0] == name:
+            student_list[i] = student[:subject_idx] + (new_grade,) + student[subject_idx+1:]
+            print(f"Updated {name}'s grade in subject {subject_idx} to {new_grade}")
+            break
+    return student_list
+
+
 students = [
     ("Adam", 80, 67, 98),
     ("Ewa", 60, 70, 90),
@@ -39,3 +54,6 @@ average_grade(students)
 best_student(students)
 new_student = ("Zofia", 88, 78, 92)
 add_student(students, new_student)
+print(sort_students_by_avg(students))
+update_grade(students, "Ewa", 2, 75) 
+print(students)
