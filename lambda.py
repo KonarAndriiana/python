54c874fde1f9ae8077ea2bca3e960bf9d823c43a
KonarAndriiana <andriiana.konar@kosickaakademia.sk>
2024-09-26 08:28:47 +0200

    Add script to double numbers in a list using lambda function

---
+++ lambda.py
@@ -0,0 +1,10 @@
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+doubled_list = list(map(lambda x: x*2 , numbers))
+print(doubled_list)
+
+# result = list(map(lambda x: x+1 , numbers))
+# print(result)
+# # alebo 
+# result1 = map(lambda x: x+1 , numbers)
+# print(list(result1))
